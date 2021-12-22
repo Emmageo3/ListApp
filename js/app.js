559cfad8583e4bc56bdd73b5bd70cb3d6b0a4bca
Emmageo3 <kanfanyemma22@gmail.com>
2021-12-22 14:02:29 +0000

    committons

--- js/app.js
+++ js/app.js
@@ -1,28 +1,6 @@
-const tableau = [
-    {
-        prenom: emma,
-        nom: kanfany,
-        email: kanfanyemma22gmail.com,
-        date: 04/06/2001,
-        lieu: Dakar,
-        adresse: Dakar,
-        tel: 771166073,
-        niveau: intermediare,
-        photo: ""
-    }
-]
 
-const ajouter = document.getElementById('submit')
-const prenom = document.getElementById('prenom')
-const nom = document.getElementById('nom')
-const email = document.getElementById('email')
-const date = document.getElementById('date')
-const lieu = document.getElementById('lieu')
-const adresse = document.getElementById('adresse')
-const tel = document.getElementById('tel')
-const niveau = document.getElementById('niveau')
-const photo = document.getElementById('photo')
 
-ajouter.addEventListener('click', function(){
-    alert(photo)
-})
+    
+
+
+
